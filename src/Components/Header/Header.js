1599f07a9bbe2aea71c42c3a1e06ca279f59a7aa
Hitesh Verma <hitesh.verma2711@gmail.com>
2022-04-03 13:50:31 +0530

    minor fixes

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,18 +1,16 @@
 import "./Header.css";
 import React from "react";
 import { Link } from "react-router-dom";
+import LandingPageImg from "../../Assests/LandingPageImg.svg";
 
 function Header() {
   return (
     <div className="main-content flex-center-center flex-col">
       <h1>Take notes swiftly</h1>
-      <img
-        src="https://encrypted-tbn2.gstatic.com/images?q=tbn:ANd9GcQNoTk4hSaFoZmyigIsDrrO1o-DoIssEbao5kIirV6xVEF1mwUy"
-        alt="landing-page-img"
-      />
       <Link to={"/login"}>
         <button className="btn-primary-confirm">Login</button>
       </Link>
+      <img src={LandingPageImg} alt="landing-page-img" />
     </div>
   );
 }
